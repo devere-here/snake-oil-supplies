33a034c9c5980a82fdf35c6ee28bfc6a50e1a30f
devere-here <stevendeverehere@gmail.com>
2018-02-26 18:32:34 -0500

    end of day 1 started category page component

---
+++ client/components/categoryPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {logout} from '../store'
+
+const CategoryPage = (props) => {
+  return (
+    <div>
+      {props.categories.map((category) => {
+        return (
+          <Link path={'/category/health'}><CategoryInstance category={category} /></Link>
+        )
+      })}
+    </div>
+  )
+}
