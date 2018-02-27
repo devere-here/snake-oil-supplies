0a21f8a1625c9400ca2d60f13008f2bfa46fe331
Michael Sam <michaelhongsam@gmail.com>
2018-02-27 14:44:22 -0500

    merged updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-<<<<<<< HEAD
-  entry: ['babel-polyfill', './client/index.js'],
-=======
   mode: isDev ? 'development' : 'production',
   entry: [
     '@babel/polyfill', // enables async-await
     './client/index.js'
   ],
->>>>>>> master
   output: {
     path: __dirname,
     filename: './public/bundle.js'
