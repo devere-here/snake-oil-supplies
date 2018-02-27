155593585fd049cd5ca44a8046c735e2d36f90b7
devere-here <stevendeverehere@gmail.com>
2018-02-27 11:16:55 -0500

    start of day 2

---
+++ client/components/catogoryInstance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {logout} from '../store'
+
+
+const CategoryInstance = (props) => {
+
+}
