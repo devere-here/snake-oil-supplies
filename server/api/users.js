f3f8c2b335b958c5517c8dff10b62b36b87aed41
Michael Sam <michaelhongsam@gmail.com>
2018-02-27 19:14:43 -0500

    isAdmin works for CRUD users

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const { User } = require('../db/models')
 const asyncHandler = require('express-async-handler')
 module.exports = router
 
@@ -18,35 +18,51 @@ const _isAdmin = req => req.user.isAdmin
 const _isSelf = req => req.user.id === req.requestedUser.id
 
 // module.exports = {
-  const isLoggedIn = function(req, res, next) {
-    _isLoggedIn(req) ? next() : deny(next)
-  }
+const isLoggedIn = function (req, res, next) {
+  _isLoggedIn(req) ? next() : deny(next)
+}
 
-  const isAdmin = function(req, res, next) {
-    _isLoggedIn(req) && _isAdmin(req) ? next() : deny(next)
-  }
+const isAdmin = function (req, res, next) {
+  _isLoggedIn(req) && _isAdmin(req) ? next() : deny(next)
+}
 
-  const isSelf = function(req, res, next) {
-    _isLoggedIn(req) && _isSelf(req) ? next() : deny(next)
-  }
+const isSelf = function (req, res, next) {
+  _isLoggedIn(req) && _isSelf(req) ? next() : deny(next)
+}
 // }
 
 
 router.param('id', asyncHandler(async (req, res, next, id) => {
- try {
-   const user = await User.findById(id, {attributes: ['id', 'email', 'isAdmin']})
-   if (!user) {
-     const err = new Error('NOT FOUND')
-     err.status = 401
-     next(err)
-   } else {
-     req.requestedUser = user
-   }
- }
- catch (err){
-   console.error(err)
- }
+  try {
+    const user = await User.findById(id, { attributes: ['id', 'email', 'isAdmin'] })
+    if (!user) {
+      const err = new Error('NOT FOUND')
+      err.status = 401
+      next(err)
+    } else {
+      req.requestedUser = user
+      next()
+    }
+  }
+  catch (err) {
+    console.error(err)
+  }
 }))
+
+// router.param('id', (req, res, next, id) => {
+//   User.findById(id, { attributes: ['id', 'email', 'isAdmin'] })
+//     .then(user => {
+//       if (!user) {
+//         const err = new Error('Not found!')
+//         err.status = 401
+//         next(err)
+//       } else {
+//         req.requestedUser = user
+//         next()
+//       }
+//     })
+//     .catch(next)
+// })
 
 router.get('/', (req, res, next) => {
   User.findAll({
@@ -61,4 +77,15 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', isSelf, (req, res, next) => {
   res.json(req.requestedUser)
+})
+
+router.put('/:id', isSelf, (req, res, next) => {
+  req.requestedUser.update(req.body)
+    .then(user => res.json(user))
+    .catch(next)
+})
+router.delete('/:id', isAdmin, (req, res, next) => {
+  req.requestedUser.destroy()
+    .then(user => res.sendStatus(204))
+    .catch(next)
 })
