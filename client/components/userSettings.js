7d15f1630d8268f0dd86445d01aa40f187f27af1
Marko Lisonek <mlisonek98@gmail.com>
2018-03-03 15:36:55 -0500

    feat(userSettings displays all user info on front end)

--- client/components/userSettings.js
+++ client/components/userSettings.js
@@ -5,58 +5,53 @@ import filterObj from 'filter-obj';
 
 
 
-class UserSettings extends Component {
-  constructor(props) {
-    super(props)
-    this
+function UserSettings(props) {
+
+  const user = props.user
+  let userSections = []
+  const sections = ['credit', 'billing', 'address', 'profile']
+  for (let section of sections) {
+    if (section !== 'profile') {
+      userSections.push(filterObj(user, key => key.startsWith(section)));
+    } else {
+      userSections.push(filterObj(user, key => key === 'email' || key === 'phone'));
+    }
   }
-  render() {
-    const user = this.props.user
-    let userSections = []
-    const sections = ['credit', 'billing', 'address', 'profile']
-    for (let section of sections) {
-      if (section !== 'profile') {
-        userSections.push(filterObj(user, key => key.startsWith(section)));
-      } else {
-        userSections.push(filterObj(user, key => key === 'email' || key === 'phone'));
+  const userSectionsMap = new Map()
+  for (let j = 0; j < sections.length; j++) {
+    userSectionsMap.set(userSections[j], sections[j])
+  }
+
+  return (
+    <div>
+      <h1>Your Information</h1>
+      {
+        userSections.map(userSection => (
+        <div key={Math.floor(Math.random() * 1000)}>
+        <h3>
+          {userSectionsMap.get(userSection)}
+        </h3>
+          <form onSubmit={this.submitHandler}>
+            <ul type="none">
+              {
+                Object.keys(userSection).map(detail => (
+                  <li key={detail}>
+                  <label>{detail.charAt(0).toUpperCase() + detail.replace(/([A-Z])/g, ' $1').trim().slice(1)}</label>
+                    <h3>{userSection[detail]}</h3>
+                  </li>
+                ))
+              }
+            </ul>
+          </form>
+        </div>
+        ))
       }
-    }
-    const userSectionsMap = new Map()
-    for (let j = 0; j < sections.length; j++) {
-      userSectionsMap.set(userSections[j], sections[j])
-    }
+      <Link to="/settings/updateSettings">
+        <button>Update Information</button>
+      </Link>
+    </div>
+  )
 
-    return (
-      <div>
-        <h1>Your Information</h1>
-        {
-          userSections.map(userSection => (
-          <div key={Math.floor(Math.random() * 1000)}>
-          <h3>
-            {userSectionsMap.get(userSection)}
-          </h3>
-            <form onSubmit={this.submitHandler}>
-              <ul type="none">
-                {
-                  Object.keys(userSection).map(detail => (
-                    <li key={detail}>
-                    <label>{detail.charAt(0).toUpperCase() + detail.replace(/([A-Z])/g, ' $1').trim().slice(1)}</label>
-                      <h3>{userSection[detail]}</h3>
-                    </li>
-                  ))
-                }
-              </ul>
-            </form>
-          </div>
-          ))
-        }
-        <Link to="/settings/updateSettings">
-          <button>Update Information</button>
-        </Link>
-      </div>
-    )
-
-  }
 }
 
 const mapState = state => ({user: state.user})
